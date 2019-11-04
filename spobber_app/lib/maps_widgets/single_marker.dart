8b470874365a4c299b921e780b53834c8f388b1f
j <j>
2019-11-04 15:35:12 +0100

    workable version for ios

--- spobber_app/lib/maps_widgets/single_marker.dart
+++ spobber_app/lib/maps_widgets/single_marker.dart
@@ -47,7 +47,7 @@ class _SingleMarkerState extends State<SingleMarker> {
                   fillColor: Colors.black,
                   hintText: 'Equipment',
                   hintStyle: TextStyle(color: Colors.grey),
-                  contentPadding: EdgeInsets.only(left: 10, bottom: 15),
+                  contentPadding: EdgeInsets.only(left: 10, bottom: 5),
                    border: InputBorder.none,
                   //  border: OutlineInputBorder(borderRadius: BorderRadius.circular(24.0)),
                 ),
