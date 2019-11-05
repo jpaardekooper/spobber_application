27e18406ff210634ddbc9d901b086607f2f4aeba
Velorexe <david.bruschke@gmail.com>
2019-11-05 11:14:13 +0100

    Added NetworkManager class

--- spobber_app/lib/data/global_variable.dart
+++ spobber_app/lib/data/global_variable.dart
@@ -1,4 +1,3 @@
-import "package:flutter/material.dart";
 import 'package:spobber_app/data/place_response.dart';
 import 'package:spobber_app/data/marker_detail.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
