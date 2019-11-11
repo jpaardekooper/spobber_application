87842680b14a475bd42194361715798ba1b9e992
Velorexe <david.bruschke@gmail.com>
2019-11-11 12:23:12 +0100

    Added ObjectComment class in commentmodel.dart

--- spobber_app/lib/network/commentmodel.dart
+++ spobber_app/lib/network/commentmodel.dart
@@ -1,15 +1,18 @@
 class ObjectComment {
   final String username;
   final String content;
+  final DateTime time;
 
   ObjectComment({
     this.username,
     this.content,
+    this.time,
   });
 
   ObjectComment fromJson(Map<String, dynamic> json) {
     return ObjectComment(
         username: json['username'],
-        content: json['content']);
+        content: json['content'],
+        time: json['time']);
   }
 }
