2492eba47d3aee0f8993357398e1b86fc10a5464
Velorexe <david.bruschke@gmail.com>
2019-11-14 15:09:35 +0100

    Initial commit

--- API (VS 2019)/SpobberApi/Models/Authentication/UserRegistration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SpobberApi.Models.Authentication
-{
-    public class UserRegistration
-    {
-        public string Email { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
