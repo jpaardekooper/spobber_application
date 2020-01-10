55b31fce36b9f692f54ef83dbc907390355290da
Jasper Paardekooper <p_jazzzp@live.nl>
2020-01-10 13:02:26 +0100

    new upgraded android project

--- updated_spobber/spobber/lib/pages/tflite/models.dart
+++
@@ -1,5 +0,0 @@
-const String mobilenet = "MobileNettttt";
-const String ssd = "SSD MobileNet";
-const String yolo = "Tiny YOLOv2";
-const String posenet = "PoseNet";
-const String testHecto = "test variant";
--- updated_spobber/spobber/lib/pages/widgets/show_toast.dart
+++
@@ -1,5 +0,0 @@
-import 'package:toast/toast.dart';
-
-void showToast(String msg, context, {int duration, int gravity}) {
-    Toast.show(msg, context, duration: duration, gravity: gravity);
-  }
